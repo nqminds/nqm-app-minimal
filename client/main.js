a983d8c2a61733ed119550361162081528aa8420
toby ealden <toby.ealden@gmail.com>
2016-11-24 17:21:03 +0000

    react router wip

--- client/main.js
+++ client/main.js
@@ -1,16 +1,18 @@
 import {createApp} from "mantra-core";
 import {combineReducers} from "redux";
+import {routerReducer} from "react-router-redux";
 import framework from "nqm-app-framework";
 import initContext from "./configs/context";
 
 // modules
-import frameworkModule from "nqm-app-framework/modules/core";
+// import frameworkModule from "nqm-app-framework/modules/core";
 import coreModule from "./modules/core";
 
 // reducers
 const reducer = combineReducers({
-  ...frameworkModule.reducers,
+  // ...frameworkModule.reducers,
   ...coreModule.reducers,
+  routing: routerReducer,
 });
 
 // init context
@@ -20,7 +22,7 @@ const context = initContext({framework, reducer});
 const app = createApp(context);
 
 // load modules
-app.loadModule(frameworkModule);
+//app.loadModule(frameworkModule);
 app.loadModule(coreModule);
 
 // go
--- client/modules/core/routes.js
+++ client/modules/core/routes.js
@@ -1,6 +1,8 @@
 import React from "react";
-import {mount} from "react-mounter";
+import ReactDOM from "react-dom";
+import {Router, Route, browserHistory} from "react-router";
 import framework from "nqm-app-framework";
+import {syncHistoryWithStore} from 'react-router-redux';
 
 // Application pages
 import Home from "../core/components/home";
@@ -14,27 +16,64 @@ const Layout = framework.ui.Layout;
 const ModalLayout = framework.ui.ModalLayout;
 
 export default function(injectDeps, context, actions) {   // eslint-disable-line no-unused-vars
-  const {FlowRouter} = context;
+  const {store} = context;
+  const history = syncHistoryWithStore(browserHistory, store);
+
   const LayoutCtx = injectDeps(Layout);
   const ModalLayoutCtx = injectDeps(ModalLayout);
 
-  FlowRouter.route("/", {
-    name: "root",
-    action() {
-      mount(LayoutCtx, {
-        title: "home",
-        content: (contentStyle) => (<Home title="HOME PAGE" style={contentStyle} />),
-        sideBarContent: AppSideBar,
-      });
-    },
-  });
+  const RouterCtx = () => (
+    <Router history={history}>
+      <Route path="/" component={Home} />
+    </Router>
+  );
 
-  FlowRouter.route("/modal", {
-    name: "modal",
-    action() {
-      mount(ModalLayoutCtx, {
-        content: () => (<Modal title="about" />),
-      });
-    },
-  });
+  const BoundRoutes = injectDeps(RouterCtx);
+
+  ReactDOM.render(
+    <BoundRoutes />,
+    document.body
+  );
 }
+
+
+// import React from "react";
+// import {mount} from "react-mounter";
+// import framework from "nqm-app-framework";
+
+// // Application pages
+// import Home from "../core/components/home";
+// import Modal from "../core/containers/modal";
+
+// // Application sidebar
+// import AppSideBar from "./components/app-side-bar";
+
+// // Get layout components from the framework
+// const Layout = framework.ui.Layout;
+// const ModalLayout = framework.ui.ModalLayout;
+
+// export default function(injectDeps, context, actions) {   // eslint-disable-line no-unused-vars
+//   const {FlowRouter} = context;
+//   const LayoutCtx = injectDeps(Layout);
+//   const ModalLayoutCtx = injectDeps(ModalLayout);
+
+//   FlowRouter.route("/", {
+//     name: "root",
+//     action() {
+//       mount(LayoutCtx, {
+//         title: "home",
+//         content: (contentStyle) => (<Home title="HOME PAGE" style={contentStyle} />),
+//         sideBarContent: AppSideBar,
+//       });
+//     },
+//   });
+
+//   FlowRouter.route("/modal", {
+//     name: "modal",
+//     action() {
+//       mount(ModalLayoutCtx, {
+//         content: () => (<Modal title="about" />),
+//       });
+//     },
+//   });
+// }
