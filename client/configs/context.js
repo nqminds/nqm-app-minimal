296c3b6cbdc2096ace7a066719091afd81d1d666
toby ealden <toby.ealden@gmail.com>
2016-11-26 00:12:25 +0000

    linting

--- client/configs/context.js
+++ client/configs/context.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-underscore-dangle */
 import {Meteor} from "meteor/meteor";
 import {Tracker} from "meteor/tracker";
 import {createStore} from "redux";
 
 // Create and return the application context
-export default function ({framework, reducer}) {
+export default function({framework, reducer}) {
   return {
     Meteor,
     Tracker,
     connectionManager: framework.connectionManager,
     constants: framework.constants,
-    store: createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()), // eslint-disable-line no-underscore-dangle
+    store: createStore(reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()),
   };
 }
